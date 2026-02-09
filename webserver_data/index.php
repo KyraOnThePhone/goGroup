a2c893159afe72fa5f22fa96009bcb75e4d7278b
kyra <kymitw001@binf02p352.dmawi.de>
2026-02-09 13:39:00 +0100

    Header, Footer

--- webserver_data/index.php
+++ webserver_data/index.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Better Itslearning</title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
+<?php include 'components/head.php'; ?>
 <body>  
     <?php include 'components/header.php'; ?>
 
     <main>
 
     </main>
-    
+
     <?php include 'components/footer.php'; ?>
 </body>
 
