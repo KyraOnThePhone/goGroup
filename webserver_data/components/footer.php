271ddba56942a17debb08ae5ff2f5f8a8086fce4
kyra <kymitw001@binf02p352.dmawi.de>
2026-02-09 14:27:29 +0100

    header, footer, login page done

--- webserver_data/components/footer.php
+++ webserver_data/components/footer.php
@@ -1,8 +1,24 @@
+<footer class="page-footer main-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5>Better Itslearning</h5>
+            </div>
 
-<footer>
-    <p>© 2026 Better Itslearning. Alle Rechte vorbehalten.</p>
-    <p>Datenschutzhinweise</p>
-    <p>Impressum</p>
+            <div class="col l4 offset-l8 s12">
+                <h5>Links</h5>
+                <ul>
+                    <li><a href="#">Rechtliche Hinweise</a></li>
+                    <li><a href="#">Impressum</a></li>
+                    <li><a href="#">Über Better Itslearning</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-copyright">
+        <div class="container">
+            © 2026 Better Itslearning
+        </div>
+    </div>
 </footer>
-
-
