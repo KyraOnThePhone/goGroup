9e1802955ca8b3ed07c4ebdcb0c8abd16c2385fb
kyra <kymitw001@binf02p352.dmawi.de>
2026-02-09 09:14:45 +0100

    header

---
+++ webserver_data/components/header.php
@@ -0,0 +1,13 @@
+    <header>
+        <nav class="nav navbar brown darken1">
+  <div class="nav-wrapper">
+    <a href="#" class="brand-logo">Better Itslearning</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="#">Gruppen</a></li>
+      <li><a href="#">Kalender</a></li>
+      <li><a href="#">Nachrichten</a></li>
+    </ul>
+  </div>
+</nav>
+
+    </header>
