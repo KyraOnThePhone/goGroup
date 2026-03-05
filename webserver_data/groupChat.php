9718d4076f99a0aa5d73e67d2a4d5f217b8a81e4
l-windhorst <lwindhorst@visiondata.de>
2026-03-05 13:41:43 +0100

    gruppenAnsicht gerüst

--- webserver_data/groupChat.php
+++ webserver_data/groupChat.php
@@ -14,8 +14,7 @@ $gruppenAvatar = "IL";
 
     <?php include 'components/groupSidebar.php'; ?>
 
-
- <main class="gruppen-page-content">
+ <main class="gruppen-chat-container">
 
         <div class="gruppen-content-header">
             <div class="gruppen-content-header-left">
@@ -30,23 +29,24 @@ $gruppenAvatar = "IL";
             </div>
         </div>
 
-        <div class="groupchat">
+        <!-- <div class="groupchat"> -->
+            <div>
             <div class="groupmessages" id="chatMessages">
-            <div class="groupmessage-group other">
+            <div class="message-group other">
+                
                 <div class="message other">Hey, hast du die Aufgabe schon erledigt? 😊</div>
-                <div class="message-time-user">14:32 Max</div>
-              
+                <div class="message-time">14:32 Max</div>
             </div>
-            <div class="groupmessage-group me">
+            <div class="message-group me">
                 <div class="message me">Ja, fast fertig! Schicke dir das gleich.</div>
-                <div class="message-time-user">14:35 Du</div>
-               
+                <div class="message-time">14:35 Du</div>  
             </div>
-            <div class="groupmessage-group other">
+            <div class="group other">
                 <div class="message other">Super, danke! 🙌</div>
-                <div class="message-time-user">14:36 Max</div>
+                <div class="message-time">14:36 Max</div>
             </div>
         </div>
+        
         <div class="group-chat-input-bar">
             <input type="text" id="chatMsgInput" placeholder="Schreibe eine Nachricht…" autocomplete="off" />
             <button class="chat-send-btn" id="chatSendBtn" type="button" title="Senden">
@@ -55,6 +55,13 @@ $gruppenAvatar = "IL";
         </div>
         </div>
     </main>
+
+    <!-- <div class= "gruppen-mitglieder-container">
+        <h1 class="gruppen-content-title">Mitglieder</h1>
+        <li>Max Mustermann</li>
+        <li>Anna Schmidt</li>
+        <li>Tobias Weber</li>
+    </div> -->
 </div>
 </body>
     </html>
---
+++ webserver_data/gruppenAnsicht.php
@@ -0,0 +1,43 @@
+<?php
+$gruppenName   = "Gruppenübersicht";
+// $gruppenMeta   = "12 Mitglieder";
+// $gruppenAvatar = "IL";
+?>
+<!DOCTYPE html>
+<html lang="de">
+<?php include 'components/head.php'; ?>
+<body>
+<?php include 'components/header.php'; ?>
+
+<div class="gruppen-page-wrapper">
+
+    <?php include 'components/groupSidebar.php'; ?>
+
+ <main class="gruppen-ansicht-container">
+
+        <div class="gruppen-content-header">
+            <div class="gruppen-content-header-left">
+                <div>
+                    <h1 class="gruppen-content-title"><?= htmlspecialchars($gruppenName) ?></h1>
+                </div>
+            </div>
+        </div>
+        
+        <div class="gruppen-popup-inner">
+                            <div class="gruppen-search-wrap">
+                                <i class="material-icons gruppen-search-icon">search</i>
+                                <input type="text" id="gruppenSearch" placeholder="Gruppe suchen…" autocomplete="off" />
+                            </div>
+
+          <ul class="gruppen-list">
+    <li class="gruppen-item"><a href="#">Gruppe 1</a></li>
+    <li class="gruppen-item"><a href="#">Gruppe 2</a></li>
+    <li class="gruppen-item"><a href="#">Gruppe 3</a></li>
+    <li class="gruppen-item"><a href="#">Gruppe 4</a></li>
+    <li class="gruppen-item"><a href="#">Gruppe 5</a></li>
+    <li class="gruppen-item"><a href="#">Gruppe 6</a></li>
+</ul>
+    </main>
+</div>
+</body>
+    </html>
